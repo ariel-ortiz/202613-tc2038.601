6edc6c586dae6c8f9dd1ca4c8240207b1c96f29f
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-08-11 10:50:33 -0600

    Add Euclidean algorithm implementation with gcd and coprime functions

---
+++ 0. Introduction/euclidean.py
@@ -0,0 +1,27 @@
+def gcd2(a: int, b: int) -> int:
+    while True:
+        r: int = a % b
+        if r == 0:
+            return b
+        a, b = b, r
+
+
+def coprimes(a: int, b: int) -> bool:
+    return gcd2(a, b) == 1
+
+
+def gcd(a: int, b: int, *rest: int) -> int:
+    result: int = gcd2(a, b)
+    for n in rest:
+        result = gcd2(result, n)
+    return result
+
+
+if __name__ == '__main__':
+    print(f'{gcd2(45, 30) = }')
+    print(f'{gcd2(6307, 1995) = }')
+    print(f'{gcd2(42, 56) = }')
+    print(f'{gcd2(99, 98) = }')
+    print(f'{coprimes(99, 98) = }')
+    print(f'{coprimes(42, 56) = }')
+    print(f'{gcd(45, 30, 20, 100) = }')
